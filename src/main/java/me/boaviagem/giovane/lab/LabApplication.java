07ac93b9ad70169714891cb6c9ba5ef1dfe44d9b
Giovane Boaviagem <giovane.ribeiro@neurotech.com.br>
2023-07-14 15:15:22 -0300

    feat: add flyweight design pattern

--- src/main/java/me/boaviagem/giovane/lab/LabApplication.java
+++ src/main/java/me/boaviagem/giovane/lab/LabApplication.java
@@ -1,12 +1,40 @@
 package me.boaviagem.giovane.lab;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import me.boaviagem.giovane.lab.music.INote;
+import me.boaviagem.giovane.lab.music.MusicNotes;
 
 @SpringBootApplication
 public class LabApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(LabApplication.class, args);
+
+		/**
+		 * FLYWEIGHT
+		 * 
+		 * Às vezes, precisamos criar várias instâncias de uma mesma classe. 
+		 * O problema é que se elas são essencialmente iguais, podemos instanciar 
+		 * esta classe de maneira estática, e utilizá-la repetidamente (exemplo da classe Fa). Assim,
+		 * ecomomizamos memória e diminuimos os pontos de manutenção.
+		 */
+
+		MusicNotes notes = new MusicNotes();
+
+		List<INote> music = Arrays.asList(
+			notes.get("do"),
+			notes.get("re"),
+			notes.get("mi"),
+			notes.get("fa"),
+			notes.get("fa"),
+			notes.get("fa")
+		);
+
+		new Piano().play(music);
 	}
 
 }
